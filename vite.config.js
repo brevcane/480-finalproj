4d9c4ead818e30f23fd8b0c12330df2ada673596
brev <brev_horton@outlook.com>
2025-04-11 16:24:55 -0400

    changes

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,8 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
+  build: {
+    outDir: 'docs', // Change the output directory to 'docs'
+  },
   base: '/480-finalproj/locar-vite-project/',
 })
